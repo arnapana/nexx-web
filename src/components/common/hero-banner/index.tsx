04ffe6c71f3c1497a231d5fe3648efc736f58703
PATRADANAI NAKPIMAY <patradanai@digio.co.th>
2022-04-23 23:16:13 +0700

    add blur image

--- src/components/common/hero-banner/index.tsx
+++ src/components/common/hero-banner/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
-import {ImageLoader}from '@components/common'
+import { ImageLoader } from '@components/common'
 import { NextPage } from 'next'
 
 interface IHeroBanner {
@@ -15,7 +15,7 @@ export const HeroBanner: NextPage<IHeroBanner> = ({ src, children, containerClas
       <div
         className={classNames(sectionClassName, `relative h-[30rem] bg-cover bg-center bg-no-repeat`, '2xl:h-[40rem]')}
       >
-        <ImageLoader src={src} layout="fill" objectFit='cover' priority={true}/> 
+        <ImageLoader src={src} layout='fill' objectFit='cover' priority={true} placeholder='blur' />
         <div className={classNames(containerClassName, 'absolute top-[16%] left-[13%]')}>{children}</div>
       </div>
     </section>
