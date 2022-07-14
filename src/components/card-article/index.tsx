69eaf52c9d15982de4173d276cbdbddc581ef5dc
PATRADANAI NAKPIMAY <patradanai@digio.co.th>
2022-07-14 15:46:00 +0700

    add a tag

--- src/components/card-article/index.tsx
+++ src/components/card-article/index.tsx
@@ -25,8 +25,10 @@ export const CardArticle: React.FC<Props> = ({ val, containerClassName, innerCla
         <div className='md:mr-3'>
           <div className='relative mx-auto w-full h-[200px] md:w-[225px] md:h-[147px]'>
             {val?.imgSrc ? (
-              <Link href={{ pathname: '/article/[slug]', query: { slug: val.slug } }} passHref>
-                <ImageLoader className='rounded-xl' layout='fill' src={val?.imgSrc} objectFit='cover' quality={100} />
+              <Link href={{ pathname: '/article/[slug]', query: { slug: val.slug } }}>
+                <a>
+                  <ImageLoader className='rounded-xl' layout='fill' src={val?.imgSrc} objectFit='cover' quality={100} />
+                </a>
               </Link>
             ) : (
               <div className='absolute bg-slate-400 rounded-md animate-pulse' style={{ width: '100%', height: '100%' }} />
@@ -35,16 +37,18 @@ export const CardArticle: React.FC<Props> = ({ val, containerClassName, innerCla
         </div>
         {/* Content */}
         <div className='flex flex-col justify-between w-full md:pl-1 xl:pb-0'>
-          <Link href={{ pathname: '/article/[slug]', query: { slug: val.slug } }} passHref>
-            <div>
-              <div className='mt-2 mb-1 md:mt-0'>
-                <p className='font-prompts font-medium line-clamp-2 md:text-base'>{val?.title}</p>
-              </div>
-              <div className='mb-2'>
-                <p className='font-sarabun text-sm font-normal text-[#5D5D5D] line-clamp-3 '>{val?.description}</p>
-              </div>
+          <div>
+            <Link href={{ pathname: '/article/[slug]', query: { slug: val.slug } }}>
+              <a>
+                <div className='mt-2 mb-1 cursor-pointer md:mt-0'>
+                  <p className='font-prompts font-medium line-clamp-2 md:text-base'>{val?.title}</p>
+                </div>
+              </a>
+            </Link>
+            <div className='mb-2'>
+              <p className='font-sarabun text-sm font-normal text-[#5D5D5D] line-clamp-3 '>{val?.description}</p>
             </div>
-          </Link>
+          </div>
 
           <div className='flex space-x-5'>
             {_.map(val.categories.slice(0, 2), (v, idx) => (
